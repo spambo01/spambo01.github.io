85b87f909bfcff6b7d9dcf12f50d84c7ba4cfa0b
savvas pamboukas <savvaspamboukas@hotmail.com>
2018-06-24 21:18:09 +0300

    apply different styling in navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var myIndex = 0;
+/*var myIndex = 0;
 carousel();
 
 function carousel() {
@@ -12,6 +12,30 @@ function carousel() {
     x[myIndex-1].style.display = "block";  
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+*/
+window.onload = function() {
+  adjustNavbar();
+};
+window.onresize = function(event) {
+  adjustNavbar();
+};
+function adjustNavbar() {
+var navbarChild = document.getElementsByTagName("nav")[0];
+var backgroundImage = document.getElementsByClassName("bg-image")[0];
+if ((window.innerWidth < 575) && (backgroundImage.contains(navbarChild)))
+    {
+        backgroundImage.removeChild(navbarChild);
+        document.body.insertBefore(navbarChild,backgroundImage);
+    }
+else if ((window.innerWidth >= 575) && (!backgroundImage.contains(navbarChild)))
+{
+   
+    document.body.removeChild(navbarChild);
+    backgroundImage.insertBefore(navbarChild,backgroundImage.children[0]);
+        }
+}
+    
+
 
 
 
